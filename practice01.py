c8b7e88de3db90197143ca9f0fba54d2fa775ac7
Shruti Ranjan Jha <72469934+shruti724@users.noreply.github.com>
2022-08-21 18:35:49 +0530

    Update practice01.py

--- practice01.py
+++ practice01.py
@@ -1,4 +1,21 @@
 # First Programme 
 print("Hello World")
 
+#list slicing
+list = [1, 2, 3, 4]
+print([0:2])
+print([:3])
+print([1:])
+print([-5:-2])
+# Prints the list by skiping one element
+print([::2])
+# Prints the list
+print([::1])
+# Reverses the list
+print([::-1])
+
+# *args and **kwargs
+
+
+
 
