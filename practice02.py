017705ff777ad3c7174b83a9c3acdb3a4bae3c74
Shruti Ranjan Jha <72469934+shruti724@users.noreply.github.com>
2022-08-22 17:50:02 +0530

    solving Trapping Rain water problem

--- practice02.py
+++ practice02.py
@@ -1,1 +1,21 @@
+# Trapping Rain water problem (using two pointers approach)
 
+class Solution:
+  def trap(self, height: List[int]) -> int:
+    
+    if not height: retun 0
+      
+    left, right = 0, len(height) - 1
+    leftMax, rightMax = height[1], height[r]
+    res = 0
+    
+    while left < right:
+      if leftMax < rightMax:
+        left += 1
+        leftMax = max(leftMax, height[left])
+        ans += leftMax - height[left]
+      else:
+        right -= 1
+        rightMax = max(rightMax, height[r])
+        ans += rightMax - height[right]
+    return ans    
